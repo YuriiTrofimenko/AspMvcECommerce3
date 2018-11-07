b53980274e235d5adb5e69702c62934532529611
YuriiTrofimenko <tyaa@ukr.net>
2018-11-07 17:20:08 +0200

    setup navigation

--- ASPNETMVC_ECommerce_3.WebUI/wwwroot/js/custom.js
+++ ASPNETMVC_ECommerce_3.WebUI/wwwroot/js/custom.js
@@ -1,4 +1,15 @@
-﻿$(document).ready(function () {
+﻿var preloaderHide = function () {
+    $('.preloader-wrapper').css('display', 'none');
+}
+
+var preloaderShow = function () {
+    $('.preloader-wrapper').css('display', 'block');
+}
+
+
+//https://material.io/design/
+//https://materializecss.com
+$(document).ready(function () {
     $('.modal').modal();
     $('.sidenav').sidenav();
     /*var modalInstance =
